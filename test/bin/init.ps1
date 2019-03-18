1facb58149fef030059122781cab6ed8b8ec38bd
Richard Kuhnt <r15ch13+git@gmail.com>
2019-03-18 13:58:05 +0100

    tests(appveyor): Add lessmsi and innounp helpers

--- test/bin/init.ps1
+++ test/bin/init.ps1
@@ -9,6 +9,22 @@ if($env:CI -eq $true) {
     Set-BuildEnvironment -Force
 }
 
+$helpersPath = 'C:\projects\helpers'
+
+if(!(Test-Path $helpersPath)) {
+    New-Item -ItemType Directory -Path $helpersPath
+}
+
+if(!(Test-Path "$helpersPath\lessmsi\lessmsi.exe")) {
+    Start-FileDownload 'https://github.com/activescott/lessmsi/releases/download/v1.6.3/lessmsi-v1.6.3.zip' -FileName "$helpersPath\lessmsi.zip"
+    7z x "$helpersPath\lessmsi.zip" -o"$helpersPath\lessmsi" -y
+}
+
+if(!(Test-Path "$helpersPath\innounp\innounp.exe")) {
+    Start-FileDownload 'https://raw.githubusercontent.com/scoopinstaller/binary-mirror/master/innounp/innounp048.rar' -FileName "$helpersPath\innounp.rar"
+    7z x "$helpersPath\innounp.rar" -o"$helpersPath\innounp" -y
+}
+
 $buildVariables = ( Get-ChildItem -Path 'Env:' ).Where( { $_.Name -match "^(?:BH|CI(?:_|$)|APPVEYOR)" } )
 $buildVariables += ( Get-Variable -Name 'CI_*' -Scope 'Script' )
 $details = $buildVariables |
